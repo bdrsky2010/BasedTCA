82c4d92154e306dd7f701082169b4bf9ff80434f
Timmy <80156515+bdrsky2010@users.noreply.github.com>
2025-01-04 23:31:53 +0900

    test: 연락처 삭제 플로우 테스트 추가
    - deleteButtonTapped 액션 테스트 추가
    - confirmDeletion 액션 테스트 추가 (연락처 삭제 및 경고창 해제 검증)

--- PracticeTCATests/ContactsFeatureTests.swift
+++ PracticeTCATests/ContactsFeatureTests.swift
@@ -14,6 +14,26 @@ import ComposableArchitecture
 @MainActor
 struct ContactsFeatureTests {
     @Test
+    func deleteContact() async {
+        let store = TestStore(initialState: ContactsFeature.State(
+            contacts: [
+                Contact(id: UUID(0), name: "Blob"),
+                Contact(id: UUID(1), name: "Blob Jr."),
+            ]
+        )) {
+            ContactsFeature()
+        }
+        
+        await store.send(.deleteButtonTapped(id: UUID(1))) {
+            $0.destination = .alert(.deleteConfirmation(id: UUID(1)))
+        }
+        await store.send(.destination(.presented(.alert(.confirmDeletion(id: UUID(1)))))) {
+            $0.contacts.remove(id: UUID(1))
+            $0.destination = nil
+        }
+    }
+    
+    @Test
     func addFlowNonExhaustive() async {
         let store = TestStore(initialState: ContactsFeature.State()) {
             ContactsFeature()
